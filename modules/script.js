90b501ca81f503afb88526b64e7b3024cd7f1bd9
leatep <leateps@gmail.com>
2022-06-25 11:17:29 +0400

    clean up old 'table' code from script

--- modules/script.js
+++ modules/script.js
@@ -1,8 +1,8 @@
 const url = 'http://localhost:9090/';
 var s_data;
 
-let _columns = 3
-let _rows = 40
+var max_amount = 100;
+var name_keys = ['id', 'name', 'amount'];
 
 async function getJson() {
     await fetch(url, {
@@ -15,43 +15,14 @@ async function getJson() {
     .then(data => s_data = data)
 }
 
-async function x2() {
-    await getJson();
-    const main_div = document.getElementById("main_div");
-    d_length = 0;
-    d_max = s_data.length -1;
-    d_keys = ["name", "amount", "speed"];
-    for (let i = 0; i < _columns; i++) {
-        var column_table = document.createElement("table");
-        main_div.appendChild(column_table); 
-        while (main_div.children[i].children.length < _rows && d_length < d_max) {
-            d_length ++;
-            main_div.children[i].appendChild(makeTd(d_keys, s_data[d_length]));
-        }
-    }
-}
-
-function makeTd(keys, data) {
-    var x = document.createElement("tr");
-    for (let i = 0; i < 3; i++) {
-        var y = document.createElement("td");
-        y.innerText = data[keys[i]];
-        x.appendChild(y);
-    }
-    return x;
-    
-}
-
 async function sleep(ms) {
     return await new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var max_amount = 100;
-var name_keys = ['id', 'name', 'amount'];
 async function makeMainElement() {
     await getJson();
     const main_div = document.getElementById("main_flex");
-    for (let i = 0; i < max_amount && i < s_data.length -1; i++) {
+    for (let i = 0; i < max_amount && i < s_data.length; i++) {
         main_div.appendChild(appendNewDiv(name_keys, s_data[i]));
     }
 }
